6938f30398076e8f14b708b3960fc638a6bf115d
Peter Shaburov <peter.shaburov@gmail.com>
2021-06-01 12:34:54 +0300

    Initial Commit

---
+++ WhatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by Peter Shaburov on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
