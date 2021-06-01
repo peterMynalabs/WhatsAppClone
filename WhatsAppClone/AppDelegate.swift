cdcd8b3b51ab4375867cbf12135e79f53fe072cb
Peter Shaburov <peter.shaburov@gmail.com>
2021-06-01 12:37:40 +0300

    First Commit

--- WhatsAppClone/AppDelegate.swift
+++ WhatsAppClone/AppDelegate.swift
@@ -9,11 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let controller = ViewController()
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
         return true
     }
 
